248e53fdfac4e8b30fcae7b239d1160a0a67ae6e
Santiago0212004 <sjbs0212@gmail.com>
2025-04-22 23:18:42 -0500

    change ip allocation to static

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,8 +39,8 @@ resource "azurerm_public_ip" "main" {
   name                = "publicip-demo"
   location            = var.location
   resource_group_name = azurerm_resource_group.main.name
-  allocation_method   = "Dynamic"
-  sku                 = "Basic"
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_network_interface" "main" {
