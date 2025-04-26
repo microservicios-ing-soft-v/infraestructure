5d7530cb6a036fd93b37a4c3fc31360ff9b39f37
Dylan Bermudez Cardona <101611405+dylanbc1@users.noreply.github.com>
2025-04-26 17:51:44 -0500

    feat: update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,31 +1,32 @@
-variable "resource_group_name" {
-  description = "Name for resource group"
-  type        = string
-}
-
 variable "location" {
-  description = "Azure region where resources will be deployed"
+  description = "The Azure region where resources will be created"
   type        = string
   default     = "East US"
 }
 
-variable "admin_username" {
-  description = "Username for the VM administrator account"
+variable "resource_group_name" {
+  description = "Name of the resource group"
   type        = string
-}
-
-variable "admin_password" {
-  description = "Password for the VM administrator account"
-  type        = string
-  sensitive   = true
 }
 
 variable "acr_name" {
-  description = "Name for the Azure Container Registry"
+  description = "Name of the Azure Container Registry"
   type        = string
 }
 
 variable "key_vault_name" {
-  description = "Name for the Azure Key Vault"
+  description = "Name of the Azure Key Vault"
   type        = string
+}
+
+variable "prefix" {
+  description = "Prefix for resource names"
+  type        = string
+  default     = "microservice"
+}
+
+variable "image_tag" {
+  description = "The tag for container images"
+  type        = string
+  default     = "latest"
 }
