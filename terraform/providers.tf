234f480761eded93d32f48a1bf1f8b51abc3f380
Dylan Bermudez Cardona <101611405+dylanbc1@users.noreply.github.com>
2025-04-27 12:57:56 -0500

    fix: update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,24 +5,6 @@ terraform {
     container_name       = "tfstate"
     key                  = "barraza.dylan.tfstate"
   }
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.84.0"  # Updated version for Container Apps support
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.5.0"
-    }
-  }
+  # Eliminar required_providers (ya está en main.tf)
 }
-
-provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy = true
-    }
-  }
-}
-
-data "azurerm_client_config" "current" {}
+# Eliminar provider "azurerm" y data "azurerm_client_config" (ya están en main.tf)
