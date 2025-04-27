8b51f1d2ee442bd8fa09ab4ef9433aba7cbeb2ce
Dylan Bermudez Cardona <101611405+dylanbc1@users.noreply.github.com>
2025-04-27 15:35:25 -0500

    fix: update conditional

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,15 +8,11 @@ output "container_app_environment_default_domain" {
 }
 
 output "frontend_url" {
-  value = var.deploy_services && contains(var.deploy_services_list, "frontend") ? 
-          try("https://${azurerm_container_app.frontend[0].latest_revision_fqdn}", "No disponible") :
-          "Pendiente de despliegue"
+  value = var.deploy_services && contains(var.deploy_services_list, "frontend") ? try("https://${azurerm_container_app.frontend[0].latest_revision_fqdn}", "No disponible") : "Pendiente de despliegue"
 }
 
 output "api_gateway_url" {
-  value = var.deploy_services && contains(var.deploy_services_list, "api-gateway") ? 
-          try("https://${azurerm_container_app.api_gateway[0].latest_revision_fqdn}", "No disponible") :
-          "Pendiente de despliegue"
+  value = var.deploy_services && contains(var.deploy_services_list, "api-gateway") ? try("https://${azurerm_container_app.api_gateway[0].latest_revision_fqdn}", "No disponible") : "Pendiente de despliegue"
 }
 
 output "zipkin_url" {
