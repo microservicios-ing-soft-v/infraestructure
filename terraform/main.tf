4e91a2a42ffdc5048d22886372cdb3e7eb4996ca
Dylan Bermudez Cardona <101611405+dylanbc1@users.noreply.github.com>
2025-04-27 17:02:48 -0500

    fix: Update app environment name main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -81,7 +81,7 @@ resource "random_string" "suffix" {
 
 # Container Apps Environment
 resource "azurerm_container_app_environment" "main" {
-  name                       = "cae-${var.prefix}-${random_string.suffix.result}"
+  name                       = var.containerapps_environment_name
   location                   = azurerm_resource_group.main.location
   resource_group_name        = azurerm_resource_group.main.name
   log_analytics_workspace_id = azurerm_log_analytics_workspace.main.id
