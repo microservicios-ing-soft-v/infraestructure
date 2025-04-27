5e981994badc9aa31d72b03b32a5507dde07efda
Dylan Bermudez Cardona <101611405+dylanbc1@users.noreply.github.com>
2025-04-27 12:58:36 -0500

    fix: update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -397,28 +397,3 @@ resource "azurerm_container_app" "log_message_processor" {
     }
   }
 }
-
-# Output values
-output "acr_login_server" {
-  value = azurerm_container_registry.main.login_server
-}
-
-output "container_app_environment_default_domain" {
-  value = azurerm_container_app_environment.main.default_domain
-}
-
-output "frontend_url" {
-  value = "https://${azurerm_container_app.frontend.latest_revision_fqdn}"
-}
-
-output "api_gateway_url" {
-  value = "https://${azurerm_container_app.api_gateway.latest_revision_fqdn}"
-}
-
-output "zipkin_url" {
-  value = "https://${azurerm_container_app.zipkin.latest_revision_fqdn}"
-}
-
-output "redis_hostname" {
-  value = azurerm_redis_cache.main.hostname
-}
