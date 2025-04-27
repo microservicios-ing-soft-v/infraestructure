8043138d88ccba5147bf035ad1c4980872d8caac
Dylan Bermudez Cardona <101611405+dylanbc1@users.noreply.github.com>
2025-04-27 15:28:46 -0500

    feat: update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,11 +8,15 @@ output "container_app_environment_default_domain" {
 }
 
 output "frontend_url" {
-  value = "https://${azurerm_container_app.frontend.latest_revision_fqdn}"
+  value = var.deploy_services && contains(var.deploy_services_list, "frontend") ? 
+          try("https://${azurerm_container_app.frontend[0].latest_revision_fqdn}", "No disponible") :
+          "Pendiente de despliegue"
 }
 
 output "api_gateway_url" {
-  value = "https://${azurerm_container_app.api_gateway.latest_revision_fqdn}"
+  value = var.deploy_services && contains(var.deploy_services_list, "api-gateway") ? 
+          try("https://${azurerm_container_app.api_gateway[0].latest_revision_fqdn}", "No disponible") :
+          "Pendiente de despliegue"
 }
 
 output "zipkin_url" {
