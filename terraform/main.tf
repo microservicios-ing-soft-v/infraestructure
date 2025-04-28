80beb856fd261e9e47781597affd54659a9fcf0d
Santiago Barraza <sjbs0212@gmail.com>
2025-04-28 03:20:17 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -103,3 +103,30 @@ resource "azurerm_role_assignment" "acr_pull" {
   role_definition_name = "AcrPull"
   scope          = azurerm_container_registry.main.id
 }
+
+resource "azurerm_key_vault" "main" {
+   name                = var.key_vault_name
+   location            = var.location
+   resource_group_name = azurerm_resource_group.main.name
+   tenant_id           = data.azurerm_client_config.current.tenant_id
+   sku_name            = "standard"
+   purge_protection_enabled    = false
+ }
+ 
+ resource "azurerm_key_vault_access_policy" "pipeline_secrets_access" {
+   key_vault_id = azurerm_key_vault.main.id
+ 
+   tenant_id    = data.azurerm_client_config.current.tenant_id
+   object_id    = data.azurerm_client_config.current.object_id
+ 
+   secret_permissions = [
+     "Get",
+     "List",
+     "Set",
+     "Delete",
+     "Recover",
+     "Backup",
+     "Restore"
+   ]
+ 
+ }
